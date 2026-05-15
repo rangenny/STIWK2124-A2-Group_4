369f5ac24c03260bebb345c84478cc60cae4ac31
Nurqaizatu Aneesa <qaizaroslan@gmail.com>
2026-05-15 17:00:22 +0800

    update: book-list

--- arl-frontend/src/app/components/book-list/book-list.ts
+++ arl-frontend/src/app/components/book-list/book-list.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+export interface Book {
+  id: string | number;
+  title: string;
+  author: string;
+  description?: string;
+}
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  standalone: true,
   templateUrl: './book-list.html',
-  styleUrl: './book-list.css',
+  styleUrls: ['./book-list.css']
 })
-export class BookList {}
+export class BookList { // <-- Changed to BookList
+  @Input() books: Book[] = [];
+  @Input() searchTerm: string = '';
+  @Input() currentPage: number = 1;
+  @Input() totalPages: number = 5;
+
+  @Output() searchChange = new EventEmitter<string>();
+  @Output() pageChange = new EventEmitter<number>();
+
+  onSearch(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.searchChange.emit(inputElement.value);
+  }
+
+  onPageChange(newPage: number): void {
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      this.pageChange.emit(newPage);
+    }
+  }
+}
